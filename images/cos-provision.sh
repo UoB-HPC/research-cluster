badfa2817255d9dbc6cb783ef8adade0cd0e5a46
Tom Lin <tom91136@gmail.com>
2024-09-22 19:32:41 +0100

    Initial cos variant support

--- images/cos-provision.sh
+++ images/cos-provision.sh
@@ -7,12 +7,71 @@ dnf install -y "https://repos.openhpc.community/OpenHPC/3/EL_9/$(arch)/ohpc-rele
 dnf config-manager --set-enabled crb
 dnf copr enable cyqsimon/micro -y
 
-# ELRepo setup for ML kernel
-rpm --import "https://www.elrepo.org/RPM-GPG-KEY-elrepo.org"
-dnf install -y "https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm"
-
 dnf update -y
-dnf install -y --enablerepo=elrepo-kernel kernel-ml kernel-ml-modules kernel-ml-devel
+
+IFS='_' read -r -a values <<<"$VARIANT"
+for value in "${values[@]}"; do
+  case $value in
+  plain)
+    dnf -y install kernel-core kernel-modules kernel-headers
+    ;;
+  ml | lt)
+    # ELRepo setup for ML kernel
+    rpm --import "https://www.elrepo.org/RPM-GPG-KEY-elrepo.org"
+    dnf install -y "https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm"
+    dnf install -y --enablerepo=elrepo-kernel "kernel-$value" "kernel-$value-modules" "kernel-$value-devel"
+    ;;
+  cuda)
+    case $(arch) in
+    aarch64) nv_arch="sbsa" ;;
+    *) nv_arch=$(arch) ;;
+    esac
+    dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/$nv_arch/cuda-rhel9.repo
+    dnf module enable -y nvidia-driver:open-dkms
+    dnf install -y nvidia-driver-cuda
+    ls /lib/modules | xargs -n1 /usr/lib/dkms/dkms_autoinstaller start
+    dkms status
+    systemctl enable nvidia-persistenced
+    ;;
+  rocm)
+    case $(arch) in
+    aarch64)
+      echo "ROCm is not supported on aarch64"
+      exit 1
+      ;;
+    esac
+
+    sudo tee /etc/yum.repos.d/amdgpu.repo <<EOF
+[amdgpu]
+name=amdgpu
+baseurl=https://repo.radeon.com/amdgpu/6.2.1/el/9.4/main/x86_64/
+enabled=1
+priority=50
+gpgcheck=1
+gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
+EOF
+    sudo tee --append /etc/yum.repos.d/rocm.repo <<EOF
+[ROCm-6.2.1]
+name=ROCm6.2.1
+baseurl=https://repo.radeon.com/rocm/el9/6.2.1/main
+enabled=1
+priority=50
+gpgcheck=1
+gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
+EOF
+    dnf install -y amdgpu-dkms
+    ls /lib/modules | xargs -n1 /usr/lib/dkms/dkms_autoinstaller start
+    dkms status
+    ;;
+  nec)
+    # TODO
+    ;;
+  *)
+    echo "Unknown variant: $value"
+    ;;
+  esac
+done
+
 dnf install -y --allowerasing --setopt=install_weak_deps=False \
   ohpc-slurm-client ipa-client \
   NetworkManager dhclient nfs-utils ipmitool openssh-clients openssh-server initscripts \
